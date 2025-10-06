c8e52feedde400ff1007ba5b3b99ace554723af2
Thilina Wick <thilina@alphabyte.biz>
2025-10-06 16:03:05 +0530

    mouse-icon-fix

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -97,7 +97,7 @@ const Index = () => {
             </div>
           </div>
         </div>
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 hidden lg:block animate-bounce">
           <div className="w-6 h-10 border-2 border-foreground/20 rounded-full flex justify-center">
             <div className="w-1 h-3 bg-primary rounded-full mt-2 animate-pulse-glow" />
           </div>
